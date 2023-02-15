2fcafe3aa01fcc2bc3aae5f57d591c0b82ed41c2
Łukasz <lukaszkogut1996@gmail.com>
2023-02-15 20:10:37 +0100

    task 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const newIngredients = document.querySelector("#ingredients");
+for(const ingredient of ingredients) {
+  const firstItem = document.createElement("li");
+firstItem.classList.add("item");
+  firstItem.textContent = ingredient;
+  newIngredients.append(firstItem);
+};
+
+
