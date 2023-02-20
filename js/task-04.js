82e864d303d68d5ad2ade9c3cdf3ccc8b1a2eee1
Łukasz <lukaszkogut1996@gmail.com>
2023-02-20 20:12:52 +0100

    repair

--- js/task-04.js
+++ js/task-04.js
@@ -1,7 +1,7 @@
-let counterValue = 0;
-const valueElement = document.getElementById('#value');
 const decrementButton = document.querySelector('[data-action="decrement"]');
 const incrementButton = document.querySelector('[data-action="increment"]');
+const valueElement = document.getElementById('#value');
+let counterValue = 0;
 
 decrementButton.addEventListener('click', () => {
   counterValue--;
